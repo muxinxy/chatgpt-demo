febf2b1884002043320405398eab7c4c5d30fd3f
muxinxy <zeng5360@gmail.com>
2023-11-10 17:47:34 +0800

    Chinese

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,9 +25,9 @@ export default defineConfig({
       registerType: 'autoUpdate',
       injectRegister: 'inline',
       manifest: {
-        name: 'ChatGPT-API Demo',
-        short_name: 'ChatGPT Demo',
-        description: 'A demo repo based on OpenAI API',
+        name: '聊天机器人',
+        short_name: '聊天机器人',
+        description: '基于OpenAI API的聊天机器人',
         theme_color: '#212129',
         background_color: '#ffffff',
         icons: [
